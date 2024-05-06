89d3bb1283d493489d08ed516e84c07bf82da749
Đỗ Minh Thuận <96808038+21012324ThuanDM@users.noreply.github.com>
2024-05-06 14:54:07 +0700

    rm

--- BT va VD/BTTH1/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION["IsLogin"] = false; // Reset login status
-session_destroy(); // Optional: Destroy all session data
-
-header("Location: login.php"); // Redirect to login page
-exit();
-?>
